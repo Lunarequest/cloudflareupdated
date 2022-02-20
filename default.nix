6b9680b163959c3ab117f49ee68f175f0292c07e
Luna D. Dragon <advaith.madhukar@gmail.com>
2022-02-20 15:22:38 +0530

    Revert "updated packaging for nixos"
    This reverts commit b41428684a3be5f2c8a4da9fcb5e5b3d193248f6.

--- default.nix
+++ default.nix
@@ -1,17 +1,14 @@
-{ stdenv, pkgs, fetchFromGitHub, rustPlatform, openssl, pkg-config, ... }:
-
-rustPlatform.buildRustPackage rec {
-  pname = "cloudflareupdated";
-  version = "93302fe83e06595583fb7ba6df2e56006df634cd";
-
-  src = "./"
-  
-  nativeBuildInputs = [ openssl pkg-config ];
-
-  cargoSha256 = "0drf5xnqin26zdyvx9n2zzgahcnrna0y56cphk2pb97qhpakvhbj";
-  verifyCargoDeps = true;
-  
-  preConfigure = ''
-    export HOME=$(mktemp -d)
-  '';
-}
+(import
+  (
+    let
+      lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+    in
+    fetchTarball {
+      url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+      sha256 = lock.nodes.flake-compat.locked.narHash;
+    }
+  )
+  {
+    src = ./.;
+  }
+).defaultNix
