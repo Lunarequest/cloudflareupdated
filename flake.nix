4b31232970e0683025cfeac92879b3119cd0b693
Luna D. Dragon <nullrequest@vivalid.net>
2022-04-10 16:46:40 +0530

    switch to naersk

--- flake.nix
+++ flake.nix
@@ -5,11 +5,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     utils.url = "github:numtide/flake-utils";
-    rust-overlay.url = "github:oxalica/rust-overlay";
-    crate2nix = {
-      url = "github:kolloch/crate2nix";
-      flake = false;
-    };
+    naersk.url = "github:nix-community/naersk";
     flake-compat-ci.url = "github:hercules-ci/flake-compat-ci";
     flake-compat = {
       url = "github:edolstra/flake-compat";
@@ -17,80 +13,36 @@
     };
   };
 
-  outputs = { self, nixpkgs, utils, rust-overlay, crate2nix, flake-compat-ci, ... }:
-    let
-      name = "cloudflareupdated";
-    in
-    utils.lib.eachDefaultSystem
-      (system:
-        let
-          # Imports
-          pkgs = import nixpkgs {
-            inherit system;
-            overlays = [
-              rust-overlay.overlay
-              (self: super: {
-                # Because rust-overlay bundles multiple rust packages into one
-                # derivation, specify that mega-bundle here, so that crate2nix
-                # will use them automatically.
-                rustc = self.rust-bin.stable.latest.default;
-                cargo = self.rust-bin.stable.latest.default;
-              })
-            ];
-          };
-          inherit (import "${crate2nix}/tools.nix" { inherit pkgs; })
-            generatedCargoNix;
+  outputs = { self, nixpkgs,flake-compat-ci, flake-compat, utils, naersk }:
+    utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages."${system}";
+      naersk-lib = naersk.lib."${system}";
+    in rec {
+      # `nix build`
+      packages.cloudflareupdated = naersk-lib.buildPackage {
+        pname = "cloudflareupdated";
+        root = ./.;
+      };
+      defaultPackage = packages.cloudflareupdated;
 
-          # Create the cargo2nix project
-          project = pkgs.callPackage
-            (generatedCargoNix {
-              inherit name;
-              src = ./.;
-            })
-            {
-              # Individual crate overrides go here
-              # Example: https://github.com/balsoft/simple-osd-daemons/blob/6f85144934c0c1382c7a4d3a2bbb80106776e270/flake.nix#L28-L50
-              defaultCrateOverrides = pkgs.defaultCrateOverrides // {
-                # The app crate itself is overriden here. Typically we
-                # configure non-Rust dependencies (see below) here.
-                ${name} = oldAttrs: {
-                  inherit buildInputs nativeBuildInputs;
-                } // buildEnvVars;
-              };
-            };
+      # `nix run`
+      apps.cloudflareupdated = utils.lib.mkApp {
+        drv = packages.cloudflareupdated;
+      };
+      defaultApp = apps.cloudflareupdated;
 
-          # Configuration for the non-Rust dependencies
-          buildInputs = with pkgs; [ openssl.dev ];
-          nativeBuildInputs = with pkgs; [ rustc cargo pkgconfig nixpkgs-fmt ];
-          buildEnvVars = {
-            PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
-          };
-        in
-        rec {
-          packages.${name} = project.rootCrate.build;
+      # `nix develop`
+      devShell = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [ rustc cargo zsh ];
+        shellHook = ''
+          test -f ~/.zshrc && exec zsh
+        '';
+      };
 
-          # `nix build`
-          defaultPackage = packages.${name};
-
-          # `nix run`
-          apps.${name} = utils.lib.mkApp {
-            inherit name;
-            drv = packages.${name};
-          };
-          defaultApp = apps.${name};
-
-          # `nix develop`
-          devShell = pkgs.mkShell
-            {
-              inherit buildInputs nativeBuildInputs;
-              RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
-              shellHook = ''
-                test -f ~/.zshrc && exec zsh
-              '';
-            } // buildEnvVars;
-         ciNix = flake-compat-ci.lib.recurseIntoFlakeWith {
+      ciNix = flake-compat-ci.lib.recurseIntoFlakeWith {
             flake = self;
-            };
-        }
-      );
+      };
+
+    });
 }
+
