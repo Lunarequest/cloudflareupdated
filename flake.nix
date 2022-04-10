8b8e62cec9d14277be57441116068e79603c5f6e
Luna D. Dragon <nullrequest@vivalid.net>
2022-04-10 18:41:44 +0530

    use oxalica overlay

--- flake.nix
+++ flake.nix
@@ -6,24 +6,29 @@
     utils.url = "github:numtide/flake-utils";
     naersk.url = "github:nix-community/naersk";
     flake-compat-ci.url = "github:hercules-ci/flake-compat-ci";
+    rust-overlay.url = "github:oxalica/rust-overlay";
     flake-compat = {
       url = "github:edolstra/flake-compat";
       flake = false;
     };
   };
 
-  outputs = { self, nixpkgs, flake-compat-ci, flake-compat, utils, naersk }:
+  outputs = { self, nixpkgs, flake-compat-ci, flake-compat, utils, naersk, rust-overlay }:
     utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages."${system}";
+        rust = rust-overlay.latest.rustChannels.stable.rust; 
         naersk-lib = naersk.lib."${system}";
+        buildEnvVars = {
+          PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+        };
       in rec {
         # `nix build`
         packages.cloudflareupdated = naersk-lib.buildPackage {
           pname = "cloudflareupdated";
           root = ./.;
           buildInputs = with pkgs; [ openssl pkgconfig ];
-        };
+        } // buildEnvVars;
         defaultPackage = packages.cloudflareupdated;
 
         # `nix run`
